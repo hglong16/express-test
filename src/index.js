2ce849d7100b8868ca7539635227c8e58b88286a
yoose-joe.tang <joe.tang@yoose.com>
2022-10-01 00:04:37 +0700

    done phase 1 express

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-import 'dotenv/config'
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 
-console.log('Hello ever running Node.js project')
+const app = express();
 
-console.log(process.env.MY_SECRET)
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello, Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+  res.send('Hello, Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+  res.send('Hello, Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+  res.send('Hello, Received a DELETE HTTP method');
+});
+app.listen(process.env.PORT, () => { console.log(`Example app listening on port ${process.env.PORT}`); });
